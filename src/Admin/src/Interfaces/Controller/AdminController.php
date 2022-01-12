c79a692c7a74ac583afc97eead08fc770d866d06
Alex <alexperegrina@gmail.com>
2022-01-12 01:52:24 +0100

    Login and home in Admin

---
+++ src/Admin/src/Interfaces/Controller/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Admin\Interfaces\Controller;
+
+use Auth\Domain\Entity\User;
+use Core\Interfaces\Controller\ApiRestController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class AdminController extends ApiRestController
+{
+    #[Route('/', name: 'admin_home', methods: ['GET'])]
+    public function home(#[CurrentUser] ?User $user): Response
+    {
+        return $this->render('@Admin/base.html.twig');
+    }
+}
---
+++ src/Admin/src/Interfaces/Controller/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Admin\Interfaces\Controller;
+
+use Core\Interfaces\Controller\ApiRestController;
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthController extends ApiRestController
+{
+    #[Route('/login', name: 'admin_auth_login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('@Admin/auth/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('/logout', name: 'admin_auth_logout', methods: ['POST'])]
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
+    }
+}
